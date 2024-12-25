5facaefe7d4d4cdab13c36ed90c04fe3b329812c
Towfiq <mohammedthawfiq00@gmail.com>
2024-12-26 00:01:59 +0600

    Set layout

--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -6,12 +6,12 @@ const Blogs = () => {
     useEffect(() =>{
         fetch("blogs.json")
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => setBlogs(data))
     },[])
-    
+
     return (
-        <div>
-            
+        <div className="md:w-2/3">
+            <h1>Blogs: {blogs.length}</h1>
         </div>
     );
 };
