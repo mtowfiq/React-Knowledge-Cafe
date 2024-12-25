58d9cd491237f7874d3e9ae28e9787ae4e1d77e8
Towfiq <mohammedthawfiq00@gmail.com>
2024-12-25 19:10:53 +0600

    Created fake json data and loaded it

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './Components/Header/Header'
+import Blogs from './Components/Blogs/Blogs'
 
 function App() {
 
   return (
     <>
       <Header></Header>
-
+      <Blogs></Blogs>
     </>
   )
 }
